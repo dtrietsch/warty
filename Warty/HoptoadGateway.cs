9cd86e2b0c071ca7402dddc1d1767fa52491124f
Dan Trietsch <dtrietsch@gmail.com>
2011-02-21 15:21:35 -0500

    notifier now accepts http request object

--- Warty/HoptoadGateway.cs
+++ Warty/HoptoadGateway.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Web;
 using System.Xml;
 
 namespace Warty
@@ -11,16 +12,21 @@ namespace Warty
     {
         private const string VERSION = "2.0";
         private const string NOTIFIER_NAME = "Warty";
-        private const string NOTIFIER_VERSION = "0.0.1";
+        private const string NOTIFIER_VERSION = "0.1.0";
         private const string NOTIFIER_URL = "https://github.com/dtrietsch/warty";
         private const string NOTIFIER_API_URL = @"http://hoptoadapp.com/notifier_api/v2/notices";
 
         public string Notify(string api_key, string environment_name, Exception e)
         {
-            return SendXmlToHoptoad(ConvertExceptionIntoNotificationXml(api_key, environment_name, e));
+            return this.Notify(api_key, environment_name, null, e);
         }
 
-        private XmlDocument ConvertExceptionIntoNotificationXml(string api_key, string environment_name, Exception e)
+        public string Notify(string api_key, string environment_name, HttpRequest http_request, Exception e)
+        {
+            return SendXmlToHoptoad(ConvertExceptionIntoNotificationXml(api_key, environment_name, http_request, e));
+        }
+
+        private XmlDocument ConvertExceptionIntoNotificationXml(string api_key, string environment_name, HttpRequest http_request, Exception e)
         {
             System.Diagnostics.StackTrace trace = new System.Diagnostics.StackTrace(e, true);
 
@@ -79,6 +85,19 @@ namespace Warty
                 method = hoptoadXml.CreateAttribute("method");
                 method.Value = trace.GetFrame(j).GetMethod().Name;
                 line.Attributes.Append(method);
+            }
+
+            if (http_request != null)
+            {
+                XmlNode request, requestUrl, requestComponent;
+                request = hoptoadXml.CreateElement("request");
+                notice.AppendChild(request);
+                requestUrl = hoptoadXml.CreateElement("url");
+                requestUrl.InnerText = http_request.RawUrl.ToString();
+                request.AppendChild(requestUrl);
+                requestComponent = hoptoadXml.CreateElement("component");
+                requestComponent.InnerText = http_request.Path.ToString();
+                request.AppendChild(requestComponent);
             }
 
             XmlNode serverEnvironment, environmentName;
